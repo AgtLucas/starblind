c4ce56f18fffc8dacd078f725a7e07b3364cfdb1
Lucas <lucas@agtlucas.com>
2015-06-13 09:14:48 -0300

    Fix typo in gulpfile and update webpack

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('js', function() {
-  gulp.src('app/assets/js/app.js')
+  gulp.src('app/assets/js/app.jsx')
     .pipe($.webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('app/build/js'));
 });
