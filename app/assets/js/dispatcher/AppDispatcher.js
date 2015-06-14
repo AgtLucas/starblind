a48912ef8d3e831f67766f7e367c60572c35f98a
Lucas <lucas@agtlucas.com>
2015-06-14 10:03:54 -0300

    Update AppDispatcher

--- app/assets/js/dispatcher/AppDispatcher.js
+++ app/assets/js/dispatcher/AppDispatcher.js
@@ -8,5 +8,13 @@ var AppDispatcher = assign({}, Dispatcher.prototype, {
    * as a view action.  Another variant here could be handleServerAction.
    * @param  {object} action The data coming from the view.
    */
+  handleViewAction: function(action) {
+    this.dispatch({
+      source: 'VIEW_ACTION',
+      action: action
+    });
+  }
 
 });
+
+export default AppDispatcher;
