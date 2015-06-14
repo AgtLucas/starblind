bd145e95c51e1febad90a91102f69719ac154809
Lucas <lucas@agtlucas.com>
2015-06-14 10:02:55 -0300

    Updata AppDispatcher

--- app/assets/js/dispatcher/AppDispatcher.js
+++ app/assets/js/dispatcher/AppDispatcher.js
@@ -1,6 +1,12 @@
-var Dispatcher = require('./Dispatcher');
-var assign = require('object-assign');
+import Dispatcher from './Dispatcher';
+import assign from 'object-assign';
 
 var AppDispatcher = assign({}, Dispatcher.prototype, {
+
+  /**
+   * A bridge function between the views and the dispatcher, marking the action
+   * as a view action.  Another variant here could be handleServerAction.
+   * @param  {object} action The data coming from the view.
+   */
 
 });
