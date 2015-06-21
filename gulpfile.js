9bc9c1e411bab61fd513cb2535d5b3444b2125b7
Lucas <AgtLucas@users.noreply.github.com>
2015-06-20 21:11:58 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
+// JS task
 gulp.task('js', function() {
   gulp.src('app/assets/js/app.jsx')
     .pipe($.webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('app/build/js'));
+});
+
+// Sass task
+gulp.task('sass', function() {
+  gulp.src('app/assets/sass/**/*.scss')
+    .pipe($.sass())
+    .pipe(gulp.dest('app/build/css'))
 });
 
 gulp.task('watch', function() {
